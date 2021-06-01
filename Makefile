2e9d6a0f67d5e6b609fd56009f7d9ffb0613cacb
Nguyen Cao <ncao@securityscorecard.io>
2021-05-31 23:24:16 -0700

    init spark

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	sh build.sh
+
+start:
+	docker compose up
+
+stop:
+	docker compose down
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ TAG=2.4.5-hadoop2.7
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
@@ -18,5 +18,5 @@ build master
 build worker
 build submit
 build java-template template/java
-build scala-template template/scala
+# build scala-template template/scala
 build python-template template/python
