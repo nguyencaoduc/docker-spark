1871a15e95372f4975a6a2a22c369d5a635491fc
Nguyen Cao <ncao@securityscorecard.io>
2021-06-21 05:40:35 -0700

    version spark 3.0.2

--- build.sh
+++ build.sh
@@ -3,13 +3,16 @@
 set -e
 
 TAG=3.0.2-hadoop3.2
+SPARK_VERSION=3.0.2
+SCALA_VERSION=2.12.12
+HADOOP_TAG=2.0.0-hadoop2.7.4-java8
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
-    docker build -t $IMAGE .
+    docker build -t $IMAGE --build-arg HADOOP_TAG=$HADOOP_TAG --build-arg TAG=$TAG --build-arg SPARK_VERSION=$SPARK_VERSION --build-arg SCALA_VERSION=$SCALA_VERSION .
     cd -
 }
 
